fbc94854805cf88f04f41e0416e5c4a7ef842298
Cynthia P <cynthiads417@gmail.com>
2023-11-06 20:33:07 -0500

    add productjs

---
+++ server/models/products.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require ('mongoose');
+
+const productSchema = new Schema ({
+    productId: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+
+    title: {
+        type: String,
+        required: true,
+    },
+
+    description: {
+        type: String,
+        required: true,
+    },
+
+    price: {
+        type: Number,
+        required: true,
+    },
+
+    image: {
+        type: String,
+        required: true,
+    },
+
+});
+
+const Product = model ('Product', productSchema);
+
+module.exports = Product;
