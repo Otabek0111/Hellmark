06c768ad91567935515ad926dd142a3838be05a2
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-11-07 21:21:00 -0500

    App.jsx updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="flex-column justify-center align-center min-100-vh bg-primary">
+    <div className="flex-column justify-center align-center min-100-vh">
       <Outlet />
     </div>
   );
