97b5fa4c9a26aaa3c1f79187685eef62bcd90157
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-11-08 21:08:59 -0500

    added productsStore.js file

---
+++ client/src/productsStore.js
@@ -0,0 +1,19 @@
+const cardArray = [
+    {id: "1",
+    title: "Card 1",
+    price: 3.00
+
+},
+{id: "2",
+    title: "Card 2",
+    price: 3.00
+
+},
+{id: "3",
+    title: "Card 3",
+    price: 3.00
+
+},
+]
+
+export default  cardArray;
