1e00e45634316b6e3b9909d201066ad2740b0fea
Otabek0111 <134247761+Otabek0111@users.noreply.github.com>
2023-11-11 15:05:21 -0500

    sign up updated

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -4,16 +4,18 @@ const Signup = () => {
 
     return (
 
-        <div className="flex w-1/2 min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="flex w-1/2 min-h-full flex-1 flex-row justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-14 w-auto rounded-full justify-items-start"
+            src="./src/assets/hellmark.png"
             alt="Your Company"
           />
+
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign Up Here
           </h2>
+          
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
