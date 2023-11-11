1eb774d4e7814971820794c32f0b53b6222a66f4
Tova Greene <tovalgreene@gmail.com>
2023-11-11 16:38:48 -0500

    integrated api calls into shop.jsx

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,64 +1,33 @@
-// import React, { Fragment, useState } from 'react';
-// import Layout from '../components/Layout';
-// import { Link } from 'react-router-dom';
-// import products from '../productsStore';
-// import Cart from './Cart';
-
-// function Shop() {
-//   const [open, setOpen] = useState(false);
-//   const [cartItems, setCartItems] = useState([]);
-
-//   const addToCartHandler = (productId) => {
-//     // Find the product by ID
-//     const productToAdd = products.find((product) => product.id === productId);
-
-//     // Add the product to the cartItems array
-//     setCartItems((prevItems) => [...prevItems, productToAdd]);
-
-//     setOpen(true);
-//   };
-
-//   return (
-// <Layout>
-//     <div>
-      
-//       {/* <h1>Welcome to the Card Store!</h1> */}
-//       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-//         {products.map((product) => (
-//           <div key={product.id} style={{ margin: '10px', border: '1px solid #ddd', padding: '10px', display: 'flex', flexDirection: 'column' }}>
-//             <h3>{product.title}</h3>
-//             <img src={product.image} alt={product.title} style={{ maxWidth: '100%', maxHeight: '150px' }} />
-//             <p>${product.price}</p>
-//             <button onClick={() => addToCartHandler(product.id)}>Add to Cart</button>
-//           </div>
-//         ))}
-//       </div>
-//       <Cart open={open} setOpen={setOpen} cartItems={cartItems} />
-//     </div>
-//     </Layout>
-//   );
-// }
-
-// export default Shop;
-import React, { Fragment, useState } from 'react';
+import React, { useState, userEffect } from 'react';
 import Layout from '../components/Layout';
-import { Link } from 'react-router-dom';
-import products from '../productsStore';
+import { getProducts } from '../utils/api';
 import Cart from './Cart';
 
-function Shop() {
-  const [open, setOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const addToCartHandler = (productId) => {
-    // Find the product by ID
-    const productToAdd = products.find((product) => product.id === productId);
+  useEffect(() => {
+    // Fetch products when the component is mounted
+    getProducts()
+      .then(data => {
+        setProducts(data); // Set the products in the state
+        setIsLoading(false); // Loading is complete
+      })
+      .catch(err => {
+        setError(err.message); // Handle any errors
+        setIsLoading(false);
+      });
+  }, []);
 
-    // Add the product to the cartItems array
-    setCartItems((prevItems) => [...prevItems, productToAdd]);
+  if (isLoading) {
+    return <div>Loading...</div>; // Display a loading text or spinner graphic
+  }
 
-    setOpen(true);
-  };
+  if (error) {
+    return <div>Error: {error}</div>; // Display an error message
+  }
 
   return (
     <Layout>
