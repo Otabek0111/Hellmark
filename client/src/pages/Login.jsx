8f3dca5237511cb8ccd4c0568ed50b51f88c5a6d
Otabek0111 <134247761+Otabek0111@users.noreply.github.com>
2023-11-11 14:30:47 -0500

    login page updated

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,6 +3,66 @@ const Login = () => {
 
     return (
 
+        /*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+<>
+{/*
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-white">
+  <body class="h-full">
+  ```
+*/}
+<div className="flex w-1/2 min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img
+      className="mx-auto h-10 w-auto"
+      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+      alt="Your Company"
+    />
+    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      Sign in to your account
+    </h2>
+  </div>
+
+  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form className="space-y-6" action="#" method="POST">
+      <div>
+        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+          Email address
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            name="email"
+            type="email"
+            autoComplete="email"
+            placeholder="Email address"
+            required
+            className="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+
+
+    </form>
+
+  </div>
+</div>
+</>
     );
   };
 
