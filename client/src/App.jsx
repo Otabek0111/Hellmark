9b94fdbb9758570d732d2d5fca16314592da8373
Tova Greene <tovalgreene@gmail.com>
2023-11-11 17:43:50 -0500

    edited App.jsx, main.jsx, etc.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,36 @@
 import './App.css';
-import { Outlet, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Removed Outlet as it's not used directly here
 import Shop from './pages/Shop';
-import "./style.css";
 import Signup from './pages/Signup';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Cart from './pages/Cart.jsx';
+import NotFound from './pages/NotFound';
 
-// Uncomment import statement below after building queries and mutations
+// If you're using Apollo Client, uncomment the imports and setup the client
 // import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
+// If using Apollo Client, instantiate it here
+// const client = new ApolloClient({
+//   uri: 'your-backend-uri',
+//   cache: new InMemoryCache(),
+// });
+
 function App() {
+  // If using Apollo Client, wrap the return statement with <ApolloProvider client={client}>
   return (
-    
-    <div className="flex-column justify-center align-center min-100-vh">
-      {/* <Outlet /> */}
-      <Outlet>
-        <Routes>
-          <Route index element={<Shop />}/>
-          <Route index element={<Signup />}/>
-        </Routes>
-      </Outlet>
-    
+    // <ApolloProvider client={client}> // Uncomment if using Apollo Client
+    <div className="flex-column justify-center align-center min-100-vh bg-gray-100">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/notfound" element={<NotFound />} />
+      </Routes>
     </div>
+    // </ApolloProvider> // Uncomment if using Apollo Client
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,51 +1,14 @@
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; // Use BrowserRouter for client-side routing
 
 import App from './App.jsx';
-import Home from './pages/Home';
-// import AboutUs from './pages/AboutUs.jsx';
-import Shop from './pages/Shop.jsx';
-import Cart from './pages/Cart.jsx';
-import NotFound from './pages/NotFound';
-import Signup from './pages/Signup.jsx';
-import Login from './pages/Login.jsx';
-
 import './index.css';
-// import './App.css';
 
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      }, {
-        path: '/AboutUs',
-        element: <AboutUs />
-      }, {
-        path: '/Shop/',
-        element: <Shop />
-      },
-      {
-        path: '/Cart/',
-        element: <Cart />
-      },
-      {
-        path: '/Signup/',
-        element: <Signup />
-      },
-      {
-        path: '/Login/',
-        element: <Login />
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+root.render(
+  <BrowserRouter> {/* Wrap App in BrowserRouter */}
+    <App />
+  </BrowserRouter>
 );
