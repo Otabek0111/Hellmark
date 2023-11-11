22486a4270f452f1f5e761bfd9a28644905066fc
Otabek0111 <134247761+Otabek0111@users.noreply.github.com>
2023-11-11 15:04:10 -0500

    login page updated

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -28,13 +28,13 @@ const Login = () => {
 */}
 <div className="flex w-1/2 min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
   <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img
-      className="mx-auto h-10 w-auto"
-      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-      alt="Your Company"
-    />
+  <img
+            className="mx-auto h-14 w-auto rounded-full justify-items-start"
+            src="./src/assets/hellmark.png"
+            alt="Your Company"
+          />
     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-      Sign in to your account
+      Login to your account
     </h2>
   </div>
 
@@ -62,11 +62,7 @@ const Login = () => {
           <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
             Password
           </label>
-          <div className="text-sm">
-            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-              Forgot password?
-            </a>
-          </div>
+
         </div>
         <div className="mt-2">
           <input
