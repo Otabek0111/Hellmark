3a58f76c402ca6a4186416b68e8525741bf226c5
Tova Greene <tovalgreene@gmail.com>
2023-11-13 22:36:10 -0500

    fixed seed.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,41 +1,52 @@
 const mongoose = require('mongoose');
-const { Product, User } = require ('../models');
+const { Product, User } = require('../models');
 require('dotenv').config();
 
 //connect to mongo db
 const productData = require('./productData.json');
 const userData = require('./userData.json');
 
-mongoose.connect ( process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
-async function seedProducts () {
+db.once('open', async () => {
   try {
     // await Product.deleteMany();
     await Product.insertMany(productData);
-
-  } catch (error) {
-    console.log(error);
-  } finally {
-    db.close();
-  }
-}
-
-async function seedUsers() {
-  try {
-    // await Product.deleteMany();
     await User.insertMany(userData);
 
   } catch (error) {
     console.log(error);
   } finally {
     db.close();
-  }
-}
+  } 
+});
 
-seedProducts();
-seedUsers();
+// async function seedProducts() {
+//   try {
+//     // await Product.deleteMany();
+//     await Product.insertMany(productData);
+
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// async function seedUsers() {
+//   try {
+//     // await Product.deleteMany();
+//     await User.insertMany(userData);
+
+//   } catch (error) {
+//     console.log(error);
+//   } finally {
+//     db.close();
+//   }
+// }
+
+// seedProducts();
+// seedUsers();
